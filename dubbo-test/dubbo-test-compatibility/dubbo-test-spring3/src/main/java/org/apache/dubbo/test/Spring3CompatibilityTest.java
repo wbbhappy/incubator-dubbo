ca8b1ed2db9256bc9bb85ecc4bc8f441e590a95d
wbbhappy <44571086+wbbhappy@users.noreply.github.com>
2019-01-25 09:26:43 +0800

    dubbo修改

--- dubbo-test/dubbo-test-compatibility/dubbo-test-spring3/src/main/java/org/apache/dubbo/test/Spring3CompatibilityTest.java
+++ dubbo-test/dubbo-test-compatibility/dubbo-test-spring3/src/main/java/org/apache/dubbo/test/Spring3CompatibilityTest.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.dubbo.test;
 
 import org.apache.dubbo.demo.DemoService;
@@ -22,52 +6,36 @@ import org.apache.dubbo.test.provider.ProviderConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.util.Assert;
-
 /**
  * Dubbo compatibility test on Spring 3.2.x
- *
  * @since 2.5.8
  */
 public class Spring3CompatibilityTest {
-
     public static void main(String[] args) {
-
         ConfigurableApplicationContext provider = startupProvider();
-
         ConfigurableApplicationContext consumer = startConsumer();
 
         ConsumerConfiguration consumerConfiguration = consumer.getBean(ConsumerConfiguration.class);
-
         DemoService demoService = consumerConfiguration.getDemoService();
-
         String value = demoService.sayHello("Mercy");
 
         Assert.isTrue("DefaultDemoService - sayHell() : Mercy".equals(value), "Test is failed!");
-
         System.out.println(value);
 
         provider.close();
         consumer.close();
-
     }
 
     private static ConfigurableApplicationContext startupProvider() {
-
         ConfigurableApplicationContext context = startupApplicationContext(ProviderConfiguration.class);
-
         System.out.println("Startup Provider ...");
-
         return context;
     }
 
     private static ConfigurableApplicationContext startConsumer() {
-
         ConfigurableApplicationContext context = startupApplicationContext(ConsumerConfiguration.class);
-
         System.out.println("Startup Consumer ...");
-
         return context;
-
     }
 
     private static ConfigurableApplicationContext startupApplicationContext(Class<?>... annotatedClasses) {
@@ -76,5 +44,4 @@ public class Spring3CompatibilityTest {
         context.refresh();
         return context;
     }
-
 }
